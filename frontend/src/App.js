f5414a8cd2373a47c87755ec64797b80bcb9f0b1
Matt Lane <lane.matthew@gmail.com>
2018-09-19 12:20:42 -0700

    adding initial components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from "react-router";
+import Signup from "./Signup";
+import Admin from "./Admin";
+import Main from "./Main";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path="/signup" component={Signup} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/" component={Main} />
+        </Switch>
       </div>
     );
   }
