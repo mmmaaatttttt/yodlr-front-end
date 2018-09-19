4b38d76df7aab14855c236e8520187a921bc8031
Matt Lane <lane.matthew@gmail.com>
2018-09-19 12:37:20 -0700

    added navigation to Main.js

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Main extends Component {
   render() {
     return (
-      <h1>Main</h1>
+      <div>
+        <h1>Main</h1>
+        <h4>Where would you like to go?</h4>
+        <p>
+          <Link to="/signup">Registration Page</Link>
+        </p>
+        <p>
+          <Link to="/admin">Admin Page</Link>
+        </p>
+      </div>
     );
   }
 }
