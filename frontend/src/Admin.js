bfe88b790f9d3bd2cac9ebffccb81348818545e4
Matt Lane <lane.matthew@gmail.com>
2018-09-19 14:51:34 -0700

    adding ability to toggle user state

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import axios from "axios";
 import UserCard from "./UserCard";
 
@@ -8,14 +8,31 @@ class Admin extends Component {
     super(props);
     this.state = {
       users: []
-    }
+    };
+    this.toggleStatus = this.toggleStatus.bind(this);
   }
 
   componentDidMount() {
     axios
       .get("http://localhost:3000/users")
       .then(res => {
-        this.setState({ users: res.data })
+        this.setState({ users: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  toggleStatus(user) {
+    let newUserState = user.state === "active" ? "pending" : "active";
+    let newUser = { ...user, state: newUserState };
+    axios
+      .put(`http://localhost:3000/users/${user.id}`, newUser)
+      .then(res => {
+        let usersCopy = [...this.state.users];
+        let userIdx = usersCopy.findIndex(u => u.id === newUser.id);
+        usersCopy[userIdx] = newUser;
+        this.setState({ users: usersCopy });
       })
       .catch(err => {
         console.log(err);
@@ -26,12 +43,22 @@ class Admin extends Component {
     return (
       <div>
         <h1>Admin</h1>
-        <p><Link to="/">Home</Link></p>
-        <p><Link to="/signup">Register</Link></p>
+        <p>
+          <Link to="/">Home</Link>
+        </p>
+        <p>
+          <Link to="/signup">Register</Link>
+        </p>
         <h4>Here are all the users:</h4>
-        {this.state.users.map(user => <UserCard key={user.id} user={user}/>)}
+        {this.state.users.map(user => (
+          <UserCard
+            key={user.id}
+            user={user}
+            toggleStatus={() => this.toggleStatus(user)}
+          />
+        ))}
       </div>
-    )
+    );
   }
 }
 
--- frontend/src/UserCard.js
+++ frontend/src/UserCard.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 class UserCard extends Component {
   render() {
-    const { firstName, lastName, email, state } = this.props.user
+    const { firstName, lastName, email, state } = this.props.user;
+    const { toggleStatus } = this.props;
     return (
       <div>
         <ul>
           <li>{firstName} {lastName}</li>
           <li>Email: {email}</li>
           <li>Status: {state}</li>
+          <li><button onClick={toggleStatus}>Change Status</button></li>
         </ul>
       </div>
     );
